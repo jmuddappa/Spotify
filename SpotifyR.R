6017be0eb5d51b8872a06babd57f25447a29c29e
Dhananjay <mudda006@umn.edu>
2017-12-09 13:51:20 -0600

    Added ShinyUI

--- SpotifyR.R
+++ SpotifyR.R
@@ -1,4 +1,4 @@
-#Spotify API Test
+#Spotify API Test with Shiny UI
 
 #install.packages("spotifyr")
 #install.packages("tidyverse")
@@ -27,9 +27,42 @@ library(tidyverse)
 Sys.setenv(SPOTIFY_CLIENT_ID = '6e7cff976750491eafd9f25577e3e12c')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = '5c925185ba3d4e6d90726be944bfe77a')
 
-artist_name <- 'joy division'
-artist <- get_artist_audio_features(artist_name)
-db <-joy %>% arrange(-valence) %>% select(track_name, valence) 
+artist_name <- 'Vicetone'
+artist_data <- get_artist_audio_features(artist_name)
+count(artist_data)
+db <-artist_data %>% arrange(-album_popularity) %>% select(album_name, album_popularity) 
+#db <- db[- grep("Remastered", db$track_name),]
+#db <- db[- grep("live", db$track_name),]
 db = as.data.frame(db[-1,])
-db
-plot(db$valence)
+unique(db)
+plot(db)
+
+#Take a user and generate a wordcloud of their favorite words in songs
+
+playlists <- get_user_playlists('lisashirai')
+playlist_tracks <- get_playlist_tracks(playlists)
+text <- unique(playlist_tracks$track_name)
+docs <- Corpus(VectorSource(text))
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+
+docs <- tm_map(docs, content_transformer(tolower))
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+docs <- tm_map(docs, removeWords, c("feat", "remix","live","radio","edit","cover","mix")) 
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, stripWhitespace)
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 20)
+
+set.seed(1234)
+wordcloud(words=d$word, freq = d$freq, min.freq = 1,
+          max.words=20, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
